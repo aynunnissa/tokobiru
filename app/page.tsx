041abfcbb69a0acfbb97f7765aa30080c8c24b94
Aynun Nissa Setiawan <aynun@GTJKTMAC-159.local>
2024-10-10 18:06:11 +0700

    feat: add Product Card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import currency from "@/utils/currency";
 import Navbar from "./components/Navbar"
+import ProductCard from "./components/ProductCard";
 import styles from "./page.module.scss"
 
 export default function Home() {
@@ -7,6 +9,19 @@ export default function Home() {
       <Navbar />
       <div style={{ marginTop: '100px' }}>
         <h1 className={styles.gogo}>Tokobiru</h1>
+        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+          {[1,2,3,4,5,6,7].map(val => <ProductCard 
+            key={val}
+            name="Jual Jaket Kulit PREMIUM buatan lokal"
+            price={currency(300000)}
+            image=""
+            hasDiscount={true}
+            discountPercent="50%"
+            discountCaption="OFF"
+            selling="1RB terjual"
+            rating="4.7"
+          />)}
+        </div>
       </div>
     </div>
   );
