ebe49961e38f43c70a26fa9c4bc4011fccf0a0dc
Aynun Nissa Setiawan <aynun@GTJKTMAC-159.local>
2024-10-11 16:40:45 +0700

    feat: implement hero carousel

---
+++ features/home/HeroBanner/index.tsx
@@ -0,0 +1,22 @@
+import styles from "./heroBanner.module.scss";
+import InfiniteCarousel from "@/components/Carousel/InifiniteCarousel";
+
+type IBanner = {
+  id: number,
+  src: string,
+  alt_image: string
+}
+
+type IRes = {
+  banners: Array<IBanner>
+}
+
+const HeroBanner = ({ banners }: IRes) => {
+  return (
+    <div className={styles.hero__wrapper}>
+      <InfiniteCarousel banners={banners} />
+    </div>
+  )
+}
+
+export default HeroBanner;
